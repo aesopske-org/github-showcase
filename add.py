6f168c015bbbe00db5a369970ef0c7571adcc4b5
Newton Mbugua <mymbugua@gmail.com>
2022-02-02 20:41:21 +0300

    changed function to subtract

--- add.py
+++ add.py
@@ -1,8 +1,8 @@
 # function that adds two nums
-def add_nums(num1, num2):
-    total = num1 + num2
+def subtract_nums(num1, num2):
+    total = num1 - num2
     print(total)
     return total
 
 
-add_nums(5,7)
+subtract_nums(5,7)
